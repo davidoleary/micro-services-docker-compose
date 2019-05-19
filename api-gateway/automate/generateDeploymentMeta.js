3bd1695c993503650f374f052303d951735bd472
David O'Leary <david.oleary@gmail.com>
2019-05-19 15:07:44 +0100

    test deploy.json gen in drone

--- api-gateway/automate/generateDeploymentMeta.js
+++ api-gateway/automate/generateDeploymentMeta.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 var git = require('git-rev-sync');
 
+console.log('--------------> before deploy.json gen');
 fs.writeFileSync('./deploy.json', JSON.stringify({
   commitSha: git.long(),
   deploymentTime: new Date(),
 }), 'utf-8');
+console.log('--------------> after deploy.json gen');
+
+
